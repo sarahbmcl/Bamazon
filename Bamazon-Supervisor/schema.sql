98ed7eae5e1c62eb26d37ef3b9b7df739d9774c2
Sarah McLaughlin <sarahbmcl@gmail.com>
2020-01-06 16:27:25 -0600

    install packages

--- Bamazon-Supervisor/schema.sql
+++ Bamazon-Supervisor/schema.sql
@@ -6,6 +6,7 @@ USE bamazon;
 CREATE TABLE products(
   item_id INT AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(45) NOT NULL,
+  product_sales DECIMAL(10,2) DEFAULT 0,
   department_name VARCHAR(45) NOT NULL,
   price DECIMAL(10,2) NOT NULL,
   stock_quantity INT(10) NOT NULL,
@@ -25,3 +26,20 @@ VALUES ("Uncharted 4", "Video Games", 49.95, 150),
   ("Mad Max: Fury Road", "Films", 25.50, 57),
   ("Monopoly", "Board Games", 30.50, 35),
   ("Yahtzee", "Board Games", 19.95, 23);
+
+ CREATE TABLE departments(
+  department_id INT AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  over_head_costs DECIMAL(10,2) NOT NULL,
+  primary key(department_id)
+);
+
+SELECT * FROM departments;
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("Video Games", 200),
+  ("Food and Drink", 100),
+  ("Apparel", 50),
+  ("Necessities", 300),
+  ("Films", 35),
+  ("Board Games", 0);
